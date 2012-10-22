476cb0f268510ab6a970f97eea514f9652118f5b
policecar <priska@23bit.net>
2012-10-23 01:38:52 +0300

    no need for redundant comments

--- src/runme.py
+++ src/runme.py
@@ -5,9 +5,7 @@ __author__ = 'policecar'
 import train_classifier as train
 import predict_class as predict
 
-# train classifier(s)
 train.train_classifier( recompute_feats=True )
 
-# predict classes for data points in test_file
 predict.predict_class( test_file='test/public_leaderboard.csv', 
-					   recompute_feats=True )
+	recompute_feats=True )
